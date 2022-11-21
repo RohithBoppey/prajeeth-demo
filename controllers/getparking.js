597ef2e5f88f12b57b8661f77c621616b26b58d5
Prajeeth <prajeeth347@gmail.com>
2022-11-22 01:01:18 +0530

    Change of get request response

--- controllers/getparking.js
+++ controllers/getparking.js
@@ -3,7 +3,7 @@ const Parking = require("../models/parkingSchema");
 exports.getparking = async (req,res)=>{
     try {
         const slots = await Parking.findById({_id:"637bc622d319c82060d9c689"});
-        res.status(201).json({data:{slots},message:"successfully"});
+        res.status(201).json({slots});
     } catch (error) {
         console.error(error);
     }

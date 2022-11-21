c1398e9c0fc768c1b52fc3ef43085adf71a4ebb6
Prajeeth <prajeeth347@gmail.com>
2022-11-22 00:58:58 +0530

    Port defining

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { dbconnection } = require("./config/database");
 dbconnection();
-app.listen(8000, () => {
-    console.log("Website running on port:8000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Website running on port:3000");
 });
